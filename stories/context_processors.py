5a2d2dda2621b8ea4215f8d2d840ef761a7a67e8
stett <stettjawa@gmail.com>
2015-07-13 05:01:39 -0400

    do something with the top_story context_processor

--- stories/context_processors.py
+++ stories/context_processors.py
@@ -1,4 +1,4 @@
-#from stories.models import Story
+from stories.models import Story
 
 
 def top_story(request):
@@ -7,6 +7,6 @@ def top_story(request):
     work on, into the context.
     """
     context = {
-        'top_story': None,
+        'top_story': Story.objects.filter(authors=request.user).order_by('?').first(),
     }
     return context
