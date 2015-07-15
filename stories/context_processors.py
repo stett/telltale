f69533bb1ca31137db3b0a39ea76dd02653671cd
stett <stettjawa@gmail.com>
2015-07-15 14:07:31 -0400

    corrected context processor error

--- stories/context_processors.py
+++ stories/context_processors.py
@@ -6,9 +6,9 @@ def top_story(request):
     Puts the current user's "top story", a selected story that they have to
     work on, into the context.
     """
-    context = {
-        'top_story': Story.objects.filter(
-            finished=False,
-            current_author=request.user).order_by('?').first(),
-    }
-    return context
+    if request.user and request.user.is_authenticated():
+        return {
+            'top_story': Story.objects.filter(
+                finished=False,
+                current_author=request.user).order_by('?').first()}
+    return {}
