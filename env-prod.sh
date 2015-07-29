ca92baa6b5f451bd25312456a3ad07a3d266840f
stett <stettjawa@gmail.com>
2015-07-29 19:02:08 -0400

    correct sh typo

--- env-prod.sh
+++ env-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtualenv
-exec env/bin/activate &&
+. env/bin/activate &&
 
 # Export dev django settings module
 export DJANGO_SETTINGS_MODULE=telltale.settings.prod
